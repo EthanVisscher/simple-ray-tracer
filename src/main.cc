b838bf8290c924e835e1d251f6028b9e5164cc5c
Ethan Visscher <ecvissch@mtu.edu>
2022-08-14 23:24:32 -0400

    Add Vec3 class and utility functions

--- src/main.cc
+++ src/main.cc
@@ -1,27 +1,22 @@
+#include "color.h"
+#include "vec3.h"
 #include <iostream>
 
+#define IMAGE_WIDTH   256
+#define IMAGE_HEIGHT  256
+
 int main() {
-  const int image_width = 256;
-  const int image_height = 256;
+  std::cout << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
 
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-  for (int j = image_height-1; j >= 0; --j) {
-    std::cerr << "\nScanlines remaining: " << j << ' ' << std::flush;
-    for (int i = 0; i < image_width; ++i) {
-        auto r = double(j) / (image_height-1);
-        auto g = 0.5;
-        auto b = double(i) / (image_width-1);
-
-        int ir = static_cast<int>(255.999 * r);
-        int ig = static_cast<int>(255.999 * g);
-        int ib = static_cast<int>(255.999 * b);
-
-        std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+  for (int j = IMAGE_HEIGHT - 1; j >= 0; --j) {
+    std::cerr << "Scanlines remaining: " << j << std::endl;
+    for (int i = 0; i < IMAGE_WIDTH; ++i) {
+      color pixel_color(double(i) / (IMAGE_WIDTH - 1),
+                        double(j) / (IMAGE_HEIGHT - 1),
+                        0.25);
+      write_color(std::cout, pixel_color);
     }
   }
-    
-  std::cerr << "\nDone\n";
 
   return 0;
 }
